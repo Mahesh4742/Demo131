8702896f4118809142efb0ca387556beda8bb396
Mahesh4742 <maheshharsha131@gmail.com>
2023-08-24 15:19:07 +0530

    adding

---
+++ sk2.py
@@ -0,0 +1,17 @@
+#-------------------------------------------------------------------------------
+# Name:        module1
+# Purpose:
+#
+# Author:      Mahesh
+#
+# Created:     17-06-2023
+# Copyright:   (c) Mahesh 2023
+# Licence:     <your licence>
+#-------------------------------------------------------------------------------
+
+int_val=4
+str_val="GPTC"
+flt_val=2.4
+print("The hash value of integer:",str(hash(int_val)))
+print("The hash value of String:",str(hash(str_val)))
+print("The hash value of float:",str(hash(flt_val)))
